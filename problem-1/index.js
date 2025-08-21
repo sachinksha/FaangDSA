08dc287fb914f9139a52c16a0f8e5698505eebde
Sachin Kiran Shah <sachinksha@gmail.com>
2025-08-21 14:15:16 +0530

    fixed problem 1 with just one loop

--- problem-1/index.js
+++ problem-1/index.js
@@ -12,37 +12,54 @@
 
             }
         }
-        return null;
     }
-    console.log('result:', getTargetSumBruteForce(nums, target));
-    console.log('result:', getTargetSumBruteForce([], target));
-    console.log('result:', getTargetSumBruteForce([11], target));
-    console.log('result:', getTargetSumBruteForce([1], target));
-    console.log('result:', getTargetSumBruteForce([5, 6], target));
-    console.log('O(n) = n2');
+    // console.log('result:', getTargetSumBruteForce(nums, target));
+    // console.log('result:', getTargetSumBruteForce([], target));
+    // console.log('result:', getTargetSumBruteForce([11], target));
+    // console.log('result:', getTargetSumBruteForce([1], target));
+    // console.log('result:', getTargetSumBruteForce([5, 6], target));
+    // console.log('O(n) = n2');
 
     const optimized_getTwoSum = function (nums, target) {
-        // console.log(`optimized_getTwoSum ${nums}, target: ${target}`);
         let map = {};
         for (let i = 0; i < nums.length; i++) {
             map[target - nums[i]] = i;
         }
-        // console.log(`map: ${JSON.stringify(map, null, 2)}`)
         for (let i = 0; i < nums.length; i++) {
             const value = nums[i];
-            // console.log(`i, value : ${i}, ${value}; maps[value]: ${map[value]}`);
             if (map[nums[i]] >= 0 && map[nums[i]] !== i) {
-                // console.log(`returning [${i}, ${map[nums[i]]}]`);
                 return [i, map[nums[i]]]
             }
         }
-        return null;
     }
-    console.log('result:', optimized_getTwoSum(nums, target));
-    console.log('result:', optimized_getTwoSum([], target));
-    console.log('result:', optimized_getTwoSum([11], target));
-    console.log('result:', optimized_getTwoSum([1], target));
-    console.log('result:', optimized_getTwoSum([5, 6], target));
-    console.log('result:', optimized_getTwoSum([3, 2, 4], 6));
+    // console.log('result:', optimized_getTwoSum(nums, target));
+    // console.log('result:', optimized_getTwoSum([], target));
+    // console.log('result:', optimized_getTwoSum([11], target));
+    // console.log('result:', optimized_getTwoSum([1], target));
+    // console.log('result:', optimized_getTwoSum([5, 6], target));
+    // console.log('result:', optimized_getTwoSum([3, 2, 4], 6));
+    // console.log('O(n) = n');
+
+
+    const master_getTwoSum = function (nums, target) {
+        // console.log(`master_getTwoSum ${nums}, target: ${target}`);
+        let map = {};
+        for (let i = 0; i < nums.length; i++) {
+            const currentMapVal = map[nums[i]];
+            if(currentMapVal >=0){
+                return [currentMapVal, i];
+            }
+            else {
+                const numberToFind = target - nums[i];
+                map[numberToFind] = i;
+            }
+        }
+    }
+    console.log('result:', master_getTwoSum(nums, target));
+    console.log('result:', master_getTwoSum([], target));
+    console.log('result:', master_getTwoSum([11], target));
+    console.log('result:', master_getTwoSum([1], target));
+    console.log('result:', master_getTwoSum([5, 6], target));
+    console.log('result:', master_getTwoSum([3, 2, 4], 6));
     console.log('O(n) = n');
 })();
