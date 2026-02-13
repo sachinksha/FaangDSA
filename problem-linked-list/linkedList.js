335bb22f4b17a5fefbe4fd26bf7688748a435f8a
Sachin Kiran Shah <sachinksha@gmail.com>
2026-02-13 11:03:29 +0530

    added code for reversing a linked list range - sub problem.

--- problem-linked-list/linkedList.js
+++ problem-linked-list/linkedList.js
@@ -7,7 +7,7 @@ const description = "user input linked list - reverse the list";
 const reverseLinkedList = (head) => {
     let prev = null;
     let currentNode = head;
-    while(currentNode) {
+    while (currentNode) {
         let next = currentNode.next;
         currentNode.next = prev;
         prev = currentNode;
@@ -17,19 +17,48 @@ const reverseLinkedList = (head) => {
 }
 
 const reverseRangeOfLinkedList = (head, m, n) => {
-    return reverseLinkedList(head);
+    if ((n - m) <= 0) {
+        return head;
+    }
+    let pos = 1;
+    let currentNode = head;
+    let prevNode = null;
+    while (pos < m) {
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+        pos++;
+    }
+    let left = prevNode;
+    let mNode = currentNode;
+    while (pos >= m && pos < n) {
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+        pos++;
+    }
+    let nNode = currentNode;
+    let right = currentNode.next;
+    nNode.next = null;
+    let reversedListHead = reverseLinkedList(mNode);
+    if (left) {
+        left.next = reversedListHead;
+    }
+    if (m === 1) {
+        head = reversedListHead;
+    }
+    mNode.next = right;
+    return head;
 }
 
 const makeLinkedListFromArray = (arr) => {
     let head = null;
     let tail = null;
     let i = 0;
-    while(i<arr.length) {
-        let node = {value: arr[i], next: null}
-        if(!head){
+    while (arr && i < arr.length) {
+        let node = { value: arr[i], next: null }
+        if (!head) {
             head = node;
         }
-        if(tail) {
+        if (tail) {
             tail.next = node;
         }
         tail = node;
@@ -41,11 +70,11 @@ const makeLinkedListFromArray = (arr) => {
 const makeArrayFromLinkedList = (head) => {
     let arr = [];
     let currentNode = head;
-    while(currentNode) {
+    while (currentNode) {
         arr.push(currentNode.value);
         currentNode = currentNode.next;
     }
     return arr;
 }
 
-module.exports = {description, reverseLinkedList, reverseRangeOfLinkedList, makeLinkedListFromArray, makeArrayFromLinkedList};
+module.exports = { description, reverseLinkedList, reverseRangeOfLinkedList, makeLinkedListFromArray, makeArrayFromLinkedList };
--- problem-linked-list/linkedList.test.js
+++ problem-linked-list/linkedList.test.js
@@ -21,48 +21,48 @@ const testParams = [
 
 const testParamsReverseRange = [
     {
-        "input1": [1, 2, 3, 4, 5, 6],
+        "input1": [1, 2, 3, 4, 5, 6, 7],
         "input2": 3,
-        "input3": 4,
-        "result": [1, 2, 4, 3, 5, 6]
+        "input3": 5,
+        "result": [1, 2, 5, 4, 3, 6, 7]
     },
     {
-        "input": [200, 300, 400, 500],
+        "input1": [200, 300, 400, 500],
         "input2": 2,
         "input3": 3,
         "result": [200, 400, 300, 500]
     },
     {
-        "input": [100, 200, 300, 400, 500],
+        "input1": [100, 200, 300, 400, 500],
         "input2": 1,
         "input3": 5,
         "result": [500, 400, 300, 200, 100]
     },
     {
-        "input": [6],
+        "input1": [6],
         "input2": 1,
         "input3": 1,
         "result": [6]
     },
     {
-        "input": [],
+        "input1": [],
         "input2": 0,
         "input3": 0,
         "result": []
     }
 ];
-// test(linkedList.description, () => {
-//     for(let i=0;i<testParamsReverseRange.length;i++) {
-//         let currentTestParams = testParamsReverseRange[i];
-//         const inputList = linkedList.makeLinkedListFromArray(currentTestParams.input1);
-//         const resultList = linkedList.reverseRangeOfLinkedList(inputList, currentTestParams.input2, currentTestParams.input3);
-//         const resultArray = linkedList.makeArrayFromLinkedList(resultList);
-//         expect(currentTestParams.result).toEqual(resultArray);
-//     }
-// });
+test(linkedList.description, () => {
+    for (let i = 0; i < testParamsReverseRange.length; i++) {
+        let currentTestParams = testParamsReverseRange[i];
+        const inputList = linkedList.makeLinkedListFromArray(currentTestParams.input1);
+        const resultList = linkedList.reverseRangeOfLinkedList(inputList, currentTestParams.input2, currentTestParams.input3);
+        const resultArray = linkedList.makeArrayFromLinkedList(resultList);
+        expect(currentTestParams.result).toEqual(resultArray);
+    }
+});
 
 test(linkedList.description, () => {
-    for(let i=0;i<testParams.length;i++) {
+    for (let i = 0; i < testParams.length; i++) {
         let currentTestParams = testParams[i];
         const inputList = linkedList.makeLinkedListFromArray(currentTestParams.input);
         const resultList = linkedList.reverseLinkedList(inputList);
