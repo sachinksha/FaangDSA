f85d797b6bc5aa17f474ca18b67ff72dccba6bbf
Sachin Kiran Shah <sachinksha@gmail.com>
2026-02-16 14:41:59 +0530

    updated code for binary search and searching range of indices of a given target using binary search again to get start and end index

--- problem-binary-search/binarySearch.js
+++ problem-binary-search/binarySearch.js
@@ -2,9 +2,7 @@
 const description = `Binary search algo implementation. Here the input array is sorted in ascending order. 
 The function returns the index of the target if found, otherwise returns -1.`;
 
-const binarySearch = (arr, target) => {
-    let left = 0;
-    let right = arr.length - 1;
+const binarySearch = (arr, left = 0, right = arr.length-1, target) => {
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
         if (arr[mid] === target) {
@@ -18,4 +16,45 @@ const binarySearch = (arr, target) => {
     return -1; // Target not found
 }
 
-module.exports = { description, binarySearch };
+//Secondary search using linear search for the range of indices of given target
+const binarySearchRange = (arr, target) => {
+    let targetIndex = binarySearch(arr, 0, arr.length-1,target);
+    if(targetIndex === -1)
+        return [-1, -1];
+    let left, right;
+    let i = targetIndex;
+    while(i>=0 && arr[i] === target){
+        left = i;
+        i--;
+    }
+    i = targetIndex;
+    while(i < arr.length && arr[i] === target){
+        right = i;
+        i++;
+    }
+    return [left, right];
+}
+
+// Secondary search using binary search for the range of indices of given target
+const binarySearchRangeByInices = (arr, left = 0, right = arr.length-1, target) =>  {
+    if(!arr.length) {
+        return [-1, -1];
+    }
+    const firstPos = binarySearch(arr, left, right, target);
+    if(firstPos === -1) {
+        return [-1, -1];
+    }
+    let leftPos, rightPos;
+    let nextLeft = firstPos;
+    while(nextLeft !== -1) {
+        leftPos = nextLeft;
+        nextLeft = binarySearch(arr, 0, nextLeft - 1, target);
+    }
+    let nextRight = firstPos;
+    while(nextRight !== -1) {
+        rightPos = nextRight;
+        nextRight = binarySearch(arr, nextRight + 1, arr.length - 1, target);
+    }
+    return [leftPos, rightPos];
+}
+module.exports = { description, binarySearch, binarySearchRange, binarySearchRangeByInices };
--- problem-binary-search/binarySearch.test.js
+++ problem-binary-search/binarySearch.test.js
@@ -1,3 +1,5 @@
+const { binarySearchRange, binarySearchRangeByInices } = require('./binarySearch');
+
 const binarySearch = require('./binarySearch').binarySearch;
 
 const testParams = [
@@ -23,10 +25,50 @@ const testParams = [
     }
 ];
 
+
+const testRangeParams = [
+    {
+        "input": [1, 2, 3, 3, 4, 4, 4, 5, 6],
+        "target": 4,
+        "result": [4, 6]
+    },
+    {
+        "input": [100, 100, 200, 300, 400, 500],
+        "target": 100,
+        "result": [0, 1]
+    },
+    {
+        "input": [6],
+        "target": 6,
+        "result": [0, 0]
+    },
+    {
+        "input": [1, 2, 3, 4, 5],
+        "target": 10,
+        "result": [-1, -1]
+    }
+];
+
 test("Binary Search - " + binarySearch.description, () => {
     for (let i = 0; i < testParams.length; i++) {
         let currentTestParams = testParams[i];
-        const result = binarySearch(currentTestParams.input, currentTestParams.target);
+        const result = binarySearch(currentTestParams.input, 0, currentTestParams.input.length-1,currentTestParams.target);
+        expect(currentTestParams.result).toEqual(result);
+    }
+});
+
+test("Binary Search Range using Linear search as secondary search; - " + binarySearch.description, () => {
+    for (let i = 0; i < testRangeParams.length; i++) {
+        let currentTestParams = testRangeParams[i];
+        const result = binarySearchRange(currentTestParams.input, currentTestParams.target);
+        expect(currentTestParams.result).toEqual(result);
+    }
+});
+
+test("Binary Search Range using Binary search again for secondary search - " + binarySearch.description, () => {
+    for (let i = 0; i < testRangeParams.length; i++) {
+        let currentTestParams = testRangeParams[i];
+        const result = binarySearchRangeByInices(currentTestParams.input, 0, currentTestParams.input.length-1, currentTestParams.target);
         expect(currentTestParams.result).toEqual(result);
     }
 });
